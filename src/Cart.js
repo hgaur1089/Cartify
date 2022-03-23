5f14032768278e916c3fb90607481d8fb05618f9
hgaur1089 <hgaur1089@gmail.com>
2022-03-23 16:25:43 +0530

    created get count function and moved the state to app component from cart component

--- src/Cart.js
+++ src/Cart.js
@@ -1,92 +1,24 @@
 import React from 'react';
 import CartItem from './CartItem';
 
-class Cart extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      products: [
-        {
-          price: 99,
-          title: 'Watch',
-          qty: 1,
-          img: '',
-          id: 1,
-        },
-        {
-          price: 999,
-          title: 'Mobile Phone',
-          qty: 10,
-          img: '',
-          id: 2,
-        },
-        {
-          price: 999,
-          title: 'Laptop',
-          qty: 4,
-          img: '',
-          id: 3,
-        },
-      ],
-    };
-  }
+const Cart = (props) => {
 
-  handleIncreaseQuantity = (product) => {
-    //console.log("increase ", product);
-    const { products } = this.state;
-    const index = products.indexOf(product);
+  const { products } = props;
+  return (
+    <div className="cart">
+      {products.map((product) => {
+        return( 
+        <CartItem 
+          product={product} 
+          key={product.id} 
+          onIncreaseQuantity={props.onIncreaseQuantity}
+          onDecreaseQuantity={props.onDecreaseQuantity}
+          onDeleteProduct={props.onDeleteProduct}
+        />);
+      })}
+    </div>
+  );
 
-    products[index].qty += 1;
-
-    this.setState({
-      //products: products
-      products 
-    })
-  }
-
-  handleDecreaseQuantity = (product) => {
-    console.log("decrease ", product);
-
-    if(product.qty === 0){
-      return;
-    }
-
-    const { products } = this.state;
-    const index = products.indexOf(product);
-
-    products[index].qty -= 1;
-
-    this.setState({
-      //products: products
-      products
-    })
-  }
-
-  handleDeleteProduct = (id) => {
-    const { products } = this.state;
-    const items = products.filter((item) => item.id !== id);
-    this.setState({
-      products: items
-    });
-  }
-
-  render() {
-    const { products } = this.state;
-    return (
-      <div className="cart">
-        {products.map((product) => {
-          return( 
-          <CartItem 
-            product={product} 
-            key={product.id} 
-            onIncreaseQuantity={this.handleIncreaseQuantity}
-            onDecreaseQuantity={this.handleDecreaseQuantity}
-            onDeleteProduct={this.handleDeleteProduct}
-          />);
-        })}
-      </div>
-    );
-  }
 }
 
 export default Cart;
