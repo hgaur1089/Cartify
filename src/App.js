0c55f40061a1ad393bb52b9c63371328832c1647
hgaur1089 <hgaur1089@gmail.com>
2022-03-23 16:04:44 +0530

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Cart from './Cart';
+import Navbar from './Navbar';
 
 export default function App() {
   return (
     <div className="App">
+      <Navbar />
       <Cart />
     </div>
   );
