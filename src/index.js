b9aecfb1c3cd9b02098dc6047db89b22c0a6866f
hgaur1089 <hgaur1089@gmail.com>
2022-03-25 16:24:38 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
+import { initializeApp } from  "firebase/app";
 
-//"firebase/app";
+//'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
+
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA9rGSbWErG6_5bcrrefsvsieX8rv3Cl9M',
